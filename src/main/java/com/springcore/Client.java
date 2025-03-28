dd881dfb13383f38779df0885bdbbc841e3a04b1
Shivang Yadav <01yadavshivang@gmail.com>
2025-03-28 14:51:14 +0530

    update

--- src/main/java/com/springcore/Client.java
+++ src/main/java/com/springcore/Client.java
@@ -1,18 +1,52 @@
 package com.springcore;
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.springcore.jdbctemplate.EmployeeDao;
 import com.springcore.model.Child;
 import com.springcore.model.Employee;
 
 public class Client {
 
 	public static void main(String[] args) {
-		ApplicationContext ioc=new ClassPathXmlApplicationContext("parent_child.xml");
-		Child e2= ioc.getBean("child",Child.class);
-		System.out.print(e2);
+		ApplicationContext ioc=new ClassPathXmlApplicationContext("applicationContext.xml");
+	EmployeeDao edao =ioc.getBean("edao",EmployeeDao.class);
+		//Employee emp1= new Employee(14,"Anand","Male",46700);
+		//edao.saveEmployee(emp1);
+	
+	//2->
+	//Employee emp1= new Employee(14,"Manish","Male",46700);
+	//edao.updateEmployee(emp1);
+	//System.out.print("Updated Successfully");
+	
+	
+	//3->
+	//edao.deleteEmployee(14);
+	
+	
+	List<Employee> allEmp=edao.getAllEmp();
+	
+	
+	for(Employee employee : allEmp) {
+		System.out.println(employee);
+	}
+	System.out.println("Get Successfully");
 		
 		
-		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 //		ApplicationContext ioc=new ClassPathXmlApplicationContext("applicationContext.xml");
 //Employee bean =ioc.getBean("emp",Employee.class);
 //System.out.println(bean);//It is printing by calling setters.
